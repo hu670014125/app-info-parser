ffc9a80be012d5bbd3d1a7887f5e2ee99b1275c3
胡秋生 <hu670014125@163.com>
2020-09-16 15:47:23 +0800

    修复部分IPA包含watch版本时解析不到iPhone/ipad版本【如：微信】

--- example/npm-use.js
+++ example/npm-use.js
@@ -1,25 +1,48 @@
 const AppInfoParser = require('../lib/index')
 const IpaParser = require('../src/ipa')
 const ApkParser = require('../src/apk')
-
-// AppInfoParser parse apk
+// 解析Android APK
 const apkInfoParser = new AppInfoParser('/Users/huqs/Desktop/微信.apk')
 apkInfoParser.parse().then(result => {
-  console.log('info ----> ', result)
-  console.log('icon base64 ----> ', result.icon)
+  let info = {
+    package: result.package,
+    versionCode: result.versionCode,
+    versionName: result.versionName,
+    minSdkVersion: result.usesSdk.minSdkVersion,
+    targetSdkVersion: result.usesSdk.targetSdkVersion,
+    name: result.application.label,
+    icon: result.icon
+  }
+  console.log('info ----> ', info)
 }).catch(e => {
   console.log('err ----> ', e)
 })
 
-// // AppInfoParser parse ipa
-// const ipaInfoParser = new AppInfoParser('../packages/test.ipa')
-// ipaInfoParser.parse().then(result => {
-//   console.log('info ----> ', result)
-//   console.log('icon base64 ----> ', result.icon)
-// }).catch(e => {
-//   console.log('err ----> ', e)
-// })
+// AppInfoParser parse ipa
+let ipaInfoParser = new AppInfoParser('/Users/huqs/Desktop/微信_7.0.15.ipa')
+// const ipaInfoParser = new AppInfoParser('/Users/huqs/Desktop/vl-ios-app.ipa')
+// const ipaInfoParser = new AppInfoParser('/Users/huqs/Desktop/as.ipa')
+// let ipaInfoParser = new AppInfoParser('/Users/huqs/Desktop/QQ_8.4.5.ipa')
+//  ipaInfoParser = new AppInfoParser('/Users/huqs/Desktop/Info.ipa')
+ipaInfoParser.parse().then(result => {
+  console.log('info ----> ', result)
+  // console.log('icon base64 ----> ', result.icon)
+}).catch(e => {
+  console.log('err ----> ', e)
+})
 //
+// const zipEntries = new AdmZip('/Users/huqs/Desktop/微信_7.0.15.ipa').getEntries()
+// zipEntries.forEach(function (zipEntry) {
+//   if (zipEntry.entryName === 'Payload/WeChat.app/Info.plist') {
+//     console.log('--------->entryName', zipEntry.entryName)
+//     let plistInfo = parsePlist(zipEntry.getData().toString())
+//     console.log(plistInfo)
+//   }
+// })
+// parseBplist.parseFile('/Users/huqs/Desktop/Info.plist', res => {
+//   console.log(res)
+// })
+
 // // IpaParser
 // const ipaParser = new IpaParser('../packages/test.ipa')
 // ipaParser.parse().then(result => {
